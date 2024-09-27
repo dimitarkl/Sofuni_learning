fd8c6f52f842eb3b4477dc79e830620873779b72
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-09-27 14:37:42 +0300

    Start server

---
+++ JS Back-End/Exam Skeleton/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.listen(5000, () => console.log('Server running on: http://localhost:5000'))
